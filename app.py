62c3e10af3ee30780aa24e890d48611b57dfcf1b
cfconor <cfconor@gmail.com>
2021-04-14 16:54:51 +0000

    added template content and test images

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 import os
+from flask import (Flask, flash, render_template,
+                   redirect, request, session, url_for)
+if os.path.exists("env/env.py"):
+    import env.env
+
+print(os.environ.get("IP"))
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
