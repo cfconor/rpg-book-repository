0c7f0d5a3a248508aef4770489fb6ed364887bba
cfconor <cfconor@gmail.com>
2021-04-15 12:35:42 +0000

    added connection to mongodb

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import (Flask, flash, render_template,
                    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env/env.py"):
     import env.env
 
@@ -8,10 +10,18 @@ print(os.environ.get("IP"))
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo_obj = PyMongo(app)
+
+
 @app.route("/")
 @app.route("/catalog")
 def catalog():
-    return render_template("catalog.html")
+    articles = list(mongo_obj.db.articles.find())
+    return render_template("catalog.html", articles=articles)
 
 
 if __name__ == "__main__":
