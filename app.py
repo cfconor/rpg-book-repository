cd3a8b553dadb1f503f4f2b5af4c4e27f0521e9d
cfconor <cfconor@gmail.com>
2021-04-16 09:33:38 +0000

    adding add_article form and functionality
    Still work to be done to implement POST function to MongoDB

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import (Flask, flash, render_template,
                    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from datetime import date
 if os.path.exists("env/env.py"):
     import env.env
 
-print(os.environ.get("IP"))
+
+
+
 
 app = Flask(__name__)
 
@@ -17,11 +20,39 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo_obj = PyMongo(app)
 
 
+
+
 @app.route("/")
 @app.route("/catalog")
 def catalog():
     articles = list(mongo_obj.db.articles.find())
     return render_template("catalog.html", articles=articles)
+
+
+@app.route("/add_article", methods=["GET", "POST"])
+def add_article():
+    # setting some default values until further functionality added
+    session["username"] = "conor"
+
+    if request.method == "POST":
+        today = date.today()
+        created_date = today.strftime("%d/%m/%Y")
+        article = {
+            "article_name": request.form.get("article_name"),
+            "article_author": request.form.get("article_author"),
+            "article_category": request.form.get("article_category"),
+            "game_system": request.form.get("game_system"),
+            "created_date": created_date,
+            "created_by": session["username"],
+            "tags": [""] # this may need to be removed if there is no clean way to implement it
+        }
+        mongo.db.tasks.insert_one(article)
+        return redirect(url_for("catalog"))
+
+
+    article_categories = mongo_obj.db.categories.find().sort("category_name", 1)
+    game_systems = mongo_obj.db.game_systems.find().sort("game_systems", 1)
+    return render_template("add_article.html", categories=article_categories, game_systems=game_systems)
 
 
 if __name__ == "__main__":
