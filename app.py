1f4ba94dab28a132d9486603e14e8b6f12885ac8
cfconor <cfconor@gmail.com>
2021-05-06 21:23:29 +0000

    turning off debug mode on app.py

--- app.py
+++ app.py
@@ -247,4 +247,4 @@ def delete_game_system(game_system_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
